b14f58c68bea24d6130af98b2939540b9755b0d5
Konstantin <konmitin.work@gmail.com>
2024-03-02 18:53:02 +0300

    Наметил структуру в index.php

---
+++ src/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ru">
+<?php
+
+include_once("components/head/head.php");
+
+?>
+<body>
+
+<div class="wrapper">
+    <?php
+        include_once("components/nav/nav.php");
+        include_once("components/header/header.php");
+    ?>
+
+    <main class="main"></main>
+
+
+    <?php
+        include_once("components/footer/footer.php");
+    ?>
+</div>
+
+<script src="./js/main.min.js"></script>
+
+</body>
+</html>
+
